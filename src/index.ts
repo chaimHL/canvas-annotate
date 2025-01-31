787696b10bb86d9e5773de732a48c3fb78e93032
chaimHL <1302906470@qq.com>
2025-01-31 21:40:54 +0800

    *优化代码，更新绘制时机

--- src/index.ts
+++ src/index.ts
@@ -4,97 +4,125 @@ enum EventEnum {
 const defaultColor = '#666'
 
 class CanvasAnnotate {
-  canvas: HTMLCanvasElement
-  ctx: CanvasRenderingContext2D | null
+  canvas: HTMLCanvasElement | null = null
+  ctx: CanvasRenderingContext2D | null = null
   img: HTMLImageElement
   shapes: BaseShape[] = []
+  width = 0
+  height = 0
+  private isDrawing = false
   constructor(canvasId: string, imgUrl: string) {
-    this.canvas = document.getElementById(canvasId) as HTMLCanvasElement
+    this.canvas = document.getElementById(canvasId) as HTMLCanvasElement ?? null
     this.ctx = this.canvas.getContext('2d')
     this.img = new Image()
-    if (!this.canvas || !this.ctx || !this.img) {
-      console.error('Canvas or ctx or img is null')
+    if (!this.canvas || !this.ctx) {
+      console.error('Canvas or Canvas context is null')
       return
     }
     this.img.onload = this.initStage.bind(this)
+    this.img.onerror = () => console.error('图片加载失败')
     this.img.src = imgUrl
   }
+
   /** 将传入的图片绘制到画布 */
   initStage() {
+    if (!this.canvas || !this.ctx || !this.img.complete) {
+      console.error('Canvas or ctx or img is not ready')
+      return
+    }
     // 计算图片的缩放比例
     const scale = Math.min(
-      this.canvas.width / this.img!.width,
-      this.canvas.height / this.img!.height
+      // 如果图片尺寸为 0（例如损坏的图片），会导致除以零的错误
+      this.canvas.width / (this.img.width !== 0 ? this.img.width : 1),
+      this.canvas.height / (this.img.height !== 0 ? this.img.height : 1)
     )
-    const width = this.img.width * scale
-    const height = this.img.height * scale
+    this.width = this.img.width * scale
+    this.height = this.img.height * scale
 
     // 清除画布
-    this.ctx!.clearRect(
+    this.ctx.clearRect(
       0,
       0,
       this.canvas.width,
       this.canvas.height
     )
 
-    // Draw the image from the top-left corner of the offscreen canvas
-    this.ctx!.drawImage(this.img, 0, 0, width, height)
 
-    // 后续绘制
+    // 绘制
     const rect0 = new Rectangle(0, this.ctx!, defaultColor, 0, 0, 100, 100)
     const rect1 = new Rectangle(1, this.ctx!, defaultColor, 300, 0, 100, 100)
     this.shapes.push(rect0, rect1)
     this.draw()
-    rect0.on(EventEnum.CLICK, (e) => {
-      const x = e.offsetX
-      const y = e.offsetY
-      const isInRegion = rect0.isPointInRegion(x, y)
-      if (isInRegion) {
-        console.log(rect0.id)
-      }
-    })
-    rect1.on(EventEnum.CLICK, (e) => {
-      const x = e.offsetX
-      const y = e.offsetY
-      const isInRegion = rect1.isPointInRegion(x, y)
-      if (isInRegion) {
-        console.log(rect1.id)
-      }
-    })
+    this.addShapeClickListener(rect0)
+    this.addShapeClickListener(rect1)
     this.handleListeners()
   }
 
+  addShapeClickListener(shape: BaseShape) {
+    shape.on(EventEnum.CLICK, (e) => {
+      if (e.offsetX !== undefined && e.offsetY !== undefined) {
+        const isInRegion = shape.isPointInRegion(e.offsetX, e.offsetY)
+        if (isInRegion) {
+          console.log(shape.id)
+        }
+      }
+    })
+  }
+
   draw() {
+    if (this.isDrawing) return;
+    this.isDrawing = true;
+    this.ctx?.clearRect(0, 0, this.width, this.height)
+    this.ctx!.drawImage(this.img, 0, 0, this.width, this.height)
     this.shapes.forEach(shape => {
       this.ctx!.fillStyle = shape.color
       this.ctx!.fill(shape.path)
     })
-
-    requestAnimationFrame(this.draw.bind(this))
+    this.isDrawing = false;
   }
 
   handleListeners() {
-    this.canvas.addEventListener(EventEnum.CLICK, (e) => {
-      this.shapes.forEach(shape => {
+    if (!this.canvas) {
+      console.error('Canvas is null')
+      return
+    }
+    const handleClick = (e: MouseEvent) => {
+      this.shapes.forEach((shape) => {
         const listeners = shape.listeners[EventEnum.CLICK]
-        if (listeners)
-          listeners.forEach(listener => {
-            listener(e)
-          })
+        if (listeners) {
+          listeners.forEach((listener) => listener(e))
+        }
       })
-    })
+      this.draw()
+    }
+    this.canvas.addEventListener(EventEnum.CLICK, handleClick)
+
+    this.canvas.addEventListener('destroy', () => {
+      this.canvas?.removeEventListener(EventEnum.CLICK, handleClick);
+    });
   }
 }
 
 /** 所有形状的抽象类 */
-abstract class BaseShape {
-  abstract id: number
-  abstract color: string
-  abstract listeners: { [key in EventEnum]?: ((e: any) => void)[] }
-  // 获取路径
-  abstract get path(): Path2D
-  // 判断点是否在路径区域内
-  abstract isPointInRegion(x: number, y: number): boolean
+class BaseShape {
+  listeners: { [key in EventEnum]?: ((e: any) => void)[] } = {}
+  constructor(public id: number, public ctx: CanvasRenderingContext2D, public color: string) { }
+
+  get path(): Path2D {
+    return new Path2D()
+  }
+
+  on(eventName: EventEnum, callback: (e: any) => void) {
+    if (!this.listeners[eventName]) {
+      this.listeners[eventName] = []
+    }
+    this.listeners[eventName]?.push(callback)
+  }
+
+  isPointInRegion(x: number, y: number): boolean {
+    return false
+  }
+
 }
 
 /** 矩形 */
@@ -109,8 +137,8 @@ class Rectangle extends BaseShape {
       }
     ]
   }
-  constructor(public id: number, public ctx: CanvasRenderingContext2D, public color: string, public x: number, public y: number, public width: number, public height: number) {
-    super()
+  constructor(id: number, ctx: CanvasRenderingContext2D, color: string, public x: number, public y: number, public width: number, public height: number) {
+    super(id, ctx, color)
   }
 
   get path(): Path2D {
@@ -119,14 +147,8 @@ class Rectangle extends BaseShape {
     return rectPath
   }
 
+  // 判断点是否在路径区域内
   isPointInRegion(x: number, y: number): boolean {
     return this.ctx.isPointInPath(this.path, x, y)
-  }
-
-  on(eventName: EventEnum, callback: (e: any) => void) {
-    if (!this.listeners[eventName]) {
-      this.listeners[eventName] = []
-    }
-    this.listeners[eventName]?.push(callback)
   }
 }
